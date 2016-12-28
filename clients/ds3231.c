edb402f7ec19fe183647ce1c4148c82413f34db7
Dan Drown <dan-android@drown.org>
2016-12-28 16:54:54 -0600

    cleanup ds3231 register code

--- clients/ds3231.c
+++ clients/ds3231.c
@@ -7,18 +7,35 @@
 
 #define I2C_ADDR 0x68
 
-#define DS3231_ADDRESS     0x68
-#define DS3231_CONTROL     0x0E
-#define DS3231_STATUSREG   0x0F
-#define DS3231_AGING       0x10
+// register offsets
+#define DS3231_CONTROL        0x0E
+#define DS3231_STATUSREG      0x0F
+#define DS3231_AGING          0x10
 #define DS3231_TEMPERATURE_HI 0x11
 #define DS3231_TEMPERATURE_LO 0x12
 
-#define DS3231_OUT_OFF 0x01
-#define DS3231_OUT_1HZ 0x00
-#define DS3231_OUT_1KHZ 0x08
-#define DS3231_OUT_4KHZ 0x10
-#define DS3231_OUT_8KHZ 0x18
+// DS3231_CONTROL bits
+#define DS3231_CTRL_ALRM1INT 0x01
+#define DS3231_CTRL_ALRM2INT 0x02
+#define DS3231_CTRL_INTCN    0x04
+#define DS3231_CTRL_RS1      0x08
+#define DS3231_CTRL_RS2      0x10
+#define DS3231_CTRL_CONVTEMP 0x20
+#define DS3231_CTRL_BBSQW    0x40
+#define DS3231_CTRL_EOSC     0x80
+
+// DS3231_STATUSREG bits
+#define DS3231_STAT_A1F   0x01
+#define DS3231_STAT_A2F   0x02
+#define DS3231_STAT_BSY   0x04
+#define DS3231_STAT_EN32K 0x08
+#define DS3231_STAT_OSF   0x80
+
+// DS3231_CTRL_RS1 / DS3231_CTRL_RS2 settings
+#define DS3231_OUT_1HZ  0x00
+#define DS3231_OUT_1KHZ DS3231_CTRL_RS1
+#define DS3231_OUT_4KHZ DS3231_CTRL_RS2
+#define DS3231_OUT_8KHZ (DS3231_CTRL_RS1|DS3231_CTRL_RS2)
 
 static uint8_t read_i2c_register(int fd, uint8_t reg) {
   write_i2c(fd, &reg, 1);
@@ -51,8 +68,10 @@ int main(int argc, char **argv) {
     printf("set square wave to 1HZ\n");
     ctrl = read_i2c_register(fd, DS3231_CONTROL);
 
-    ctrl &= ~0x04; // turn off INTCON
-    ctrl &= ~0x18; // set freq bits to 0
+    // turn off INTCON
+    ctrl &= ~DS3231_CTRL_INTCN;
+    // set RS1/RS2 to 0
+    ctrl &= ~(DS3231_CTRL_RS1|DS3231_CTRL_RS2);
     ctrl |= DS3231_OUT_1HZ;
     write_i2c_register(fd, DS3231_CONTROL, ctrl);
     exit(0);
